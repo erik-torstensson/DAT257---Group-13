7069e480a4ab520d3d6da29177b9470cdbe57cbc
swallson <samuelwallson99@gmail.com>
2020-05-24 23:22:31 +0200

    Update MSconfig.js

--- Stadtruppen/MSconfig.js
+++ Stadtruppen/MSconfig.js
@@ -5,7 +5,7 @@
 const msalConfig = {
   auth: {
     clientId: '69d2caaa-830b-44e2-9586-dc1c0ee2332a',
-    redirectUri: 'http://localhost:8000'
+    redirectUri: 'https://dat257-group13.glitch.me/Stadtruppen/'
   },
   cache: {
     cacheLocation: "sessionStorage",
